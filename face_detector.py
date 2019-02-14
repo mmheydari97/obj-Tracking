5888c7992eae1680d4b82e009704c5361e679837
mmheydari97 <heidarymm@yahoo.com>
2019-02-14 16:53:49 +0330

    face parts detection

--- face_detector.py
+++ face_detector.py
@@ -2,13 +2,22 @@ import numpy as np
 import cv2
 
 
-# Load the Haar cascade file
+# Load the Haar cascade files
 face_cascade = cv2.CascadeClassifier(
     './haar_cascade_files/haarcascade_frontalface_default.xml')
 
+eye_cascade = cv2.CascadeClassifier(
+    './haar_cascade_files/haarcascade_eye.xml')
+
+nose_cascade = cv2.CascadeClassifier(
+    './haar_cascade_files/haarcascade_mcs_nose.xml')
+
+
 # Check if the cascade file has been loaded correctly
-if face_cascade.empty():
-    raise IOError('Unable to load the face cascade classifier xml file')
+
+for a in (face_cascade, eye_cascade, nose_cascade):
+    if a.empty():
+        raise IOError('Unable to load the face cascade classifier xml file')
 
 # Initialize the video capture object
 cap = cv2.VideoCapture(0)
@@ -31,9 +40,23 @@ while True:
     # Run the face detector on the grayscale image
     face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Draw a rectangle around the face
+    # Run the eye detector on the grayscale image
+    eye_rects = eye_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Run the nose detector on the grayscale image
+    nose_rects = nose_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Draw a blue rectangle around the face
     for (x, y, w, h) in face_rects:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
+
+    # Draw a green rectangle around the eyes
+    for (x, y, w, h) in eye_rects:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    # Draw a red rectangle around the nose
+    for (x, y, w, h) in nose_rects:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
 
     # Display the output
     cv2.imshow('Face Detector', frame)
